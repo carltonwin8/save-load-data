f544bdaaaf1467e62cdd7d95845aff71e9aca2e7
Carlton Joseph <carlton.joseph@gmail.com>
2017-05-09 20:51:23 -0700

    add code for server auto refresh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Knockout And Hapi Test
+
+Start the server first and then the client via:
+`npm run server`
--- server/js/server.js
+++ server/js/server.js
@@ -10,7 +10,7 @@ function task(title, isDone) {
 }
 
 const tasks = [
-  task("Wire the money to Panama", true),
+  task("Wire the money to Panama!", true),
   task('Get hair dye, beard trimmer, dark glasses and "passport"', false),
   task("Book taxi to airport", false),
   task("Arrange for someone to look after the cat", false)
@@ -25,9 +25,6 @@ server.route({
 });
 
 server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
+    if (err) { throw err; }
     console.log(`Server running at: ${server.info.uri}`);
 });
